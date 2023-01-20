ec82efd1916c681af02fe0a5fbf45d7e1923fdfc
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-20 19:25:39 +0600

    Task_2 Ended

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("введите искомое число №1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number №2");
+int num2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(num1);
+
+if (result == num2)
+{
+    Console.WriteLine($"квадратный корень Числа №1: =  {num2}");
+}
+
+if (result != num2)
+{
+    Console.WriteLine($"квадратный корень Числа №1:  не равен числу {num2}");
+}
