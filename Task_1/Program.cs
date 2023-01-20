fb546e6f10ab6ef5606cb6a2bd83a4d22ee705a5
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-20 18:48:09 +0600

    test commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("введите число ");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1 <= 5 )
+{
+    Console.WriteLine($"Будний день");
+}
+if (6 <= num1 && num1 <= 7)
+{
+    Console.WriteLine($"выходной");
+}
+if (num1 > 7)
+{
+    Console.WriteLine($"нельзя использовать числа больше 7");
+}
